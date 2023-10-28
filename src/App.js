4d7e0c9c2a1d44906fc2337040c05c1c7fabd933
Luancss <ventobo123@gmail.com>
2023-10-28 03:25:30 -0300

    Icons from mui material

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
@@ -6,7 +7,11 @@ function App() {
   return (<ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
-    <div className="App"></div>
+    <div className="App">
+      <main className="content">
+        <Topbar/>
+      </main>
+    </div>
     </ThemeProvider>
   </ColorModeContext.Provider>
   );
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,4 +1,16 @@
+import {Box, IconButton, useTheme} from '@mui/material'
+import {useContext} from 'react'
+import { ColorModeContext, tokens } from '../../theme'
+import InputBase from '@mui/material'
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import NotificationsOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import SettingsOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import PersonOutlinedLightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import SearchIcon from '@mui/icons-material/LightModeOutlined'
+
 const Topbar = () => {
+  const theme = useTheme();
   return <div>Topbar</div>
 }
 

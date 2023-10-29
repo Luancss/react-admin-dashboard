a158e910eca71a23442169a2c1d1c73c18d92086
Luancss <ventobo123@gmail.com>
2023-10-29 01:04:29 -0300

    Topbar complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { Dashboard } from "@mui/icons-material";
 import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+
+
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -10,6 +15,9 @@ function App() {
     <div className="App">
       <main className="content">
         <Topbar/>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+        </Routes>
       </main>
     </div>
     </ThemeProvider>

0d0f1981e8783b74b302f4058cd1b7edaadbbc41
Luancss <ventobo123@gmail.com>
2023-10-30 01:39:38 -0300

    Table of contacts

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Dashboard from "./scenes/dashboard";
 import { useState } from "react";
 import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -32,8 +32,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />

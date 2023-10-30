78674df81ddae5312d2df87bc94b4fa8f558fea6
Luancss <ventobo123@gmail.com>
2023-10-30 01:14:09 -0300

    Data Grid team members complete

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -82,7 +82,21 @@ const Team = () => {
         },
         "& .name-column--cell": {
           color: colors.greenAccent[300]
-        }
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: colors.blueAccent[700],
+          borderBottom: "none",
+        },
+        "& .MuiDataGrid-virtualScroller": {
+          backGroundColor: colors.primary[400]
+        },
+        "& .MuiDataGrid-footerContainer": {
+          borderTop: "none",
+          backgroundColor: colors.blueAccent[700],
+        },
+        "& .MuiCheckbox-root": {
+          color: `${colors.greenAccent[200]} !important`,
+        },
       }}>
         <DataGrid
           rows={mockDataTeam}

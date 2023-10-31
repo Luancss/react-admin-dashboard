375892bdf5adacaab33423e04003b30348a54bd2
Luancss <ventobo123@gmail.com>
2023-10-31 18:47:19 -0300

    Invoices complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/first */
-
 import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { Routes, Route } from "react-router-dom";
@@ -8,7 +7,7 @@ import Sidebar from "./scenes/global/Sidebar";;
 import Dashboard from "./scenes/dashboard";
 import { useState } from "react";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
@@ -33,8 +32,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import { Typography, Box, useTheme} from "@mui/material";
+import { Typography, Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
-const Header = ({title, subtitle}) => {
-  const theme = useTheme()
+const Header = ({ title, subtitle }) => {
+  const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  return <Box mb='30px'>
-    <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{mb:"5px"}}>{title}</Typography>
-    <Typography variant="h5" color={colors.greenAccent[400]}>{subtitle}</Typography>
-  </Box>
-}
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ m: "0 0 5px 0" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;

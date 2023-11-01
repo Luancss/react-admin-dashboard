8cbe6f5f651f3cab907a21832597e7666b2220f2
Luancss <ventobo123@gmail.com>
2023-11-01 16:36:59 -0300

    full form page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
+
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";

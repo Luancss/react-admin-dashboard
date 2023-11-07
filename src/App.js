0c1ba4e85d512eb3dad8a4c0b0b8b9818c59f30e
Luancss <ventobo123@gmail.com>
2023-11-07 01:05:47 -0300

    BarChar from nivobar complete

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
-
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
@@ -35,8 +34,8 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              {/* <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
+              <Route path="/bar" element={<Bar />} />
+              {/* <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} /> */}
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />

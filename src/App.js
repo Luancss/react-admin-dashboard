8b371224dec0a87570100d76420458a00e1fa2f9
Luancss <ventobo123@gmail.com>
2023-11-07 16:02:45 -0300

    Pie Chart complete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Calendar from "./scenes/calendar";
 
@@ -35,8 +35,8 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} /> */}
+              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/line" element={<Line />} /> */}
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
